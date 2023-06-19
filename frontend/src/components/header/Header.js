44c8b7b87daca6647efc5084f593c99b6f6aea6a
xodnr15860 <xodnr15860@naver.com>
2023-06-19 16:49:15 +0900

    헤더

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -4,35 +4,47 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className='header-container'>
-            <nav>
-                <ul className='header-navbar'>
-                    <li><Link to='/login'>로그인</Link></li>
-                    <li><Link to='/edit'>회원가입</Link></li>
-                    <li><Link to='/cart'>장바구니</Link></li>
-                    <li><Link to='/myPage'>마이페이지</Link></li>
-                    <li><Link to='/customerService'>고객센터</Link></li>
-                </ul>
-            </nav>
-            <div>
-                 <div className='logo-container'>
-                    <Link to='/'>
-                      <img src='/아이유.webp' alt='로고' className='logo'/>
-                    </Link>
+       <header>
+        <nav className="bg-white border-black">
+            <div className="flex justify-end items-center mx-auto max-w-screen-xl p-4">
+                <div className="flex items-center">
+                    <Link to='/member/login' className="text-me hover:text-green-700 px-2">로그인</Link>
+                    <Link to='/member/edit' className="text-me hover:text-green-700 px-2" >회원가입</Link>
+                    <Link to='/app/cart' className="text-me hover:text-green-700 px-2" >장바구니</Link>
+                    <Link to='/app/mypage' className="text-me hover:text-green-700 px-2" >마이페이지</Link>
+                    <Link to='/member/login' className="text-me hover:text-green-700 px-2" >고객센터</Link>
                 </div>
             </div>
-            <hr/>
-            <div>
-                <ul className='header-category'>
-                    <li><Link to='/calorieDic'>칼로리사전</Link></li>
-                    <li><Link to='/'>일반게시판</Link></li>
-                    <li><Link to='/'>모임게시판</Link></li>
-                    <li><Link to='/goodsShop'>운동용품샵</Link></li>
-                    <li><Link to='/'></Link></li>
-                </ul>
+        </nav>
+        <div>
+            <div className='logo-container'>
+                <Link to='/'>
+                    <img src='/아이유.webp' alt='로고' className='logo'/>
+                </Link>
             </div>
-            <hr/>
-        </header>
+        </div>
+        <nav className="bg-white border-2 border-gray-400">
+            <div className="max-w-screen-xl px-4 py-4 mx-auto flex justify-center">
+                <div className="flex items-center">
+                    <ul className="flex justify-center font-medium mt-0 space-x-32 text-sm">
+                        <li>
+                            <Link to='' className="text-black hover:text-green-700" aria-current="page">칼로리사전</Link>
+                        </li>
+                        <li>
+                            <Link to='' className="text-black hover:text-green-700" aria-current="page">일반게시판</Link>
+                        </li>
+                        <li>
+                            <Link to='' className="text-black hover:text-green-700" aria-current="page">모임게시판</Link>
+                        </li>
+                        <li>
+                            <Link to='' className="text-black hover:text-green-700" aria-current="page">운동용품샵</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+       </header>
     );
 };
  
